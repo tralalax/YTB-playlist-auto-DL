d26bd70ccefb9aa2c633c0785d145cead7050bde
tralalax <timepro@gmx.com>
2023-03-07 20:46:27 +0100

    bat script done

--- YTBplaylist-autoDL.bat
+++ YTBplaylist-autoDL.bat
@@ -2,14 +2,15 @@
 python --version 3>NUL
 if errorlevel 1 goto errorNoPython
 
-
+:: start the script (main.py)
 cd YTB-playlist-Auto-DL
 
-YTB-playlist-Auto-DL\YTBplDLvenv\Scripts\activate.bat && pip install --upgrade pip && pip install -r requirements.txt && python __main__.py && deactivate
+YTB-playlist-Auto-DL\YTBplDLvenv\Scripts\activate.bat && python __main__.py && deactivate
 
+:: exit the script
+exit /b
 
-pause
-
+:: message for error if no python
 :errorNoPython
 echo.
 echo Error^: Python not installed
--- installerRunAtStartup.bat
+++ installerRunAtStartup.bat
@@ -2,8 +2,7 @@
 python --version 3>NUL
 if errorlevel 1 goto errorNoPython
 
-:: python is installed.
-:: downloading file from github
+:: downloading file from github in Start directory
 cd %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/tralalax/YTB-playlist-auto-DL/main/YTBplaylist-autoDL.bat', 'YTBplaylist-autoDL.bat')"
 
@@ -16,15 +15,20 @@ powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.github
 
 :: creating python venv
 python -m venv YTBplDLvenv
-YTBplDLvenv\Scripts\activate.bat
+
+cd YTB-playlist-Auto-DL
 
 :: install dependencies
-YTBplDLvenv install -r requirements.txt
+YTB-playlist-Auto-DL\YTBplDLvenv\Scripts\activate.bat && pip install --upgrade pip && pip install -r requirements.txt && deactivate
+
+echo Installation finished
+echo config file is in %appdata%\Microsoft\Windows\Start Menu\Programs\Startup\YTB-playlist-Auto-DL
+echo check github for more info : https://github.com/tralalax/YTB-playlist-auto-DL
 
 :: exit the installer
-::exit /b
-pause
+exit /b
 
+:: message for error if no python
 :errorNoPython
 echo.
 echo Error^: Python not installed
